2e383bca754a7abe4250a989f8d14a6bcb5df717
Paul Griego <paulgriego029@gmail.com>
2022-04-05 21:14:33 -0600

    added basic functions

---
+++ cal.js
@@ -0,0 +1,17 @@
+function add (a,b) {
+  return sum = a+b;
+}
+function subtract (a,b) {
+  return sum = a-b;
+}
+function multiply (a,b) {
+  return sum = a*b;
+} 
+function divide (a,b) {
+  return sum = (a)/(b);
+}
+function operate (a,b) {
+ return sum = add(a,b)
+}
+
+
